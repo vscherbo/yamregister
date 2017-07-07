8998d8997a423a35fe0de8a09ca3668c178c2edf
Vladimir Scherbo <vscherbo@gmail.com>
2017-07-07 11:16:26 +0300

    правильный порядок order_uid, order_id; новое поле Назначение в реестре; триггер внесён в скрипт

--- sql/cr-yamregister.sql
+++ sql/cr-yamregister.sql
@@ -1,6 +1,6 @@
 CREATE TABLE arc_energo.yamregister (
 	yam_id int8 NOT NULL,
-	order_id text NOT NULL,
+	order_uid text NOT NULL,
 	amount numeric NULL,
 	currency varchar NULL,
 	net_amount numeric NULL,
@@ -8,10 +8,17 @@ CREATE TABLE arc_energo.yamregister (
 	yam_wallet varchar NULL,
 	description varchar NULL,
 	payment_type varchar NULL,
+	payment_purpose varchar NULL,
 	yaregister_date date NULL,
+	order_id text,
 	CONSTRAINT yamregister_pk PRIMARY KEY (yam_id),
 	CONSTRAINT yamregister_un UNIQUE (order_id)
 )
 WITH (
 	OIDS=FALSE
 ) ;
+
+create trigger "tr_yamregister_BI" before insert
+on yamregister
+for each row execute procedure fntr_yamoney_order_id();
+
