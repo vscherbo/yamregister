8ae15ede767a8682b825dcc34fc0016c11d60d05
Vladimir Scherbo <vscherbo@gmail.com>
2017-08-01 15:18:50 +0300

    обработка списка товаров в реестре; logrotate

--- bin/get-yam-daily-registry.sh
+++ bin/get-yam-daily-registry.sh
@@ -43,17 +43,26 @@ then
     RC=$?
     case $RC in
        0) logmsg INFO "One or more messages were successfully retrieved." 
+          exit_rc=1 # work
           ;;
        1) logmsg INFO "There was no mail."
-          exit 0
+          exit_rc=0 # skip
           ;;
        *) logmsg $RC "fetchmail completed."
-          exit $RC
+          exit_rc=$RC # unexpected RC
           ;;
     esac
 
-    rm -f $CSV_DIR/smime*.p7s
+    find $CSV_DIR -type f -name 'smime*.p7s' -delete
     find $CSV_DIR -type f -name 'yamregister*' -size 0c -delete
+
+    # clean logs without mail
+    grep -l 'There was no mail' $LOG_DIR/* |xargs --no-run-if-empty rm
+    
+    if [ $exit_rc -ne 1 ]
+    then
+       exit $exit_rc
+    fi
 fi # DO_FETCH
 
 PG_COPY_SCRIPT=$CSV_DATA/pg-COPY-registry-$DT.sql
@@ -87,7 +96,8 @@ EODATE
      PAY_DT=${loc_year}-${loc_mon}-${loc_day}
      csv_name=$PAY_DT-`namename $txt`
      PG_CSV=$CSV_DATA/$csv_name.csv
-     grep 'kipspb.ru' $txt |sed -e 's/; /;/g' -e "s/$/$reg_date;/" > $PG_CSV
+     #grep 'kipspb.ru' $txt |sed -e 's/; /;/g' -e "s/$/$reg_date;/" > $PG_CSV
+     awk '/kipspb\.ru/ {N=split($0,arr,";"); for (i=1;i<=9;i++) printf "%s;", arr[i]; print ";"}' $txt |sed -e 's/; /;/g' -e "s/$/$reg_date;/" > $PG_CSV
      mv $txt $CSV_DATA/$PAY_DT-`namename $txt`
 
      echo "\COPY yamregister FROM '"$PG_CSV"' WITH ( FORMAT CSV, HEADER false, DELIMITER ';') ;" >> $PG_COPY_SCRIPT
@@ -180,8 +190,8 @@ then
 
 fi
 
-# clean logs without mail
-grep -l 'There was no mail' $LOG_DIR/* |xargs rm
+/usr/sbin/logrotate --state get-yam-daily-registry.state get-yam-daily-registry.conf
+cat $LOG >> $LOG_DIR/get-yam-daily-registry.log
 
 popd
 
