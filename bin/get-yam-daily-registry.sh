1901d1714fb632a4b5bf18ea9c4b6c650f536930
Vladimir Scherbo <vscherbo@gmail.com>
2021-01-26 16:05:25 +0300

    обработка возвратов; несколько извещений; доработка по рекомендациям

--- bin/get-yam-daily-registry.sh
+++ bin/get-yam-daily-registry.sh
@@ -1,36 +1,41 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 . /usr/local/bin/bashlib
 
-DT=`date +%F_%H_%M_%S`
+DT=$(date +%F_%H_%M_%S)
 #############
 #DO=echo
 DO=''
-PG_SRV=vm-pg
+PG_SRV=vm-pg.arc.world
+DIR_NAME=$(dirname "$0")
+if [[ "$DIR_NAME" =~ .*devel.* ]]
+then
+    PG_SRV=vm-pg-devel.arc.world
+fi 
 #############
 
-[ +$1 = +fetch ] && DO_FETCH='YES' || DO_FETCH='NO'
+[ +"$1" = +fetch ] && DO_FETCH='YES' || DO_FETCH='NO'
 
 #1. received email are processed by procmail and ripmime
 # result is the csv file in directory specified with .procmailrc 
 # i.e.  ripmime -i - -d /path/to/mailbox 
-CSV_DIR=`grep yamregister ~/.procmailrc | awk -F '-d' '{print $2}' | awk '{print $1}'`
-[ +$CSV_DIR = + ] && { echo CSV_DIR unassigned, exiting; exit 123; }
+CSV_DIR=$(grep yamregister ~/.procmailrc | awk -F '-d' '{print $2}' | awk '{print $1}')
+[ +"$CSV_DIR" = + ] && { echo CSV_DIR unassigned, exiting; exit 123; }
 LOG_DIR=$CSV_DIR/logs
 CSV_DATA=$CSV_DIR/01-data
 CSV_ARCH=$CSV_DIR/99-archive
 ARCHIVE_DEPTH=60
 
-[ -d $CSV_DIR ] || mkdir -p $CSV_DIR
-[ -d $LOG_DIR ] || mkdir -p $LOG_DIR
-[ -d $CSV_DATA ] || mkdir -p $CSV_DATA
-[ -d $CSV_ARCH ] || mkdir -p $CSV_ARCH
+[ -d "$CSV_DIR" ] || mkdir -p "$CSV_DIR"
+[ -d "$LOG_DIR" ] || mkdir -p "$LOG_DIR"
+[ -d "$CSV_DATA" ] || mkdir -p "$CSV_DATA"
+[ -d "$CSV_ARCH" ] || mkdir -p "$CSV_ARCH"
 
-find $CSV_ARCH -type f -mtime +$ARCHIVE_DEPTH -delete # -exec rm -f {} \+
-find $LOG_DIR -type f -mtime +$ARCHIVE_DEPTH -delete # -exec rm -f {} \+
+find "$CSV_ARCH" -type f -mtime +$ARCHIVE_DEPTH -delete # -exec rm -f {} \+
+find "$LOG_DIR" -type f -mtime +$ARCHIVE_DEPTH -delete # -exec rm -f {} \+
 
-LOG=$LOG_DIR/$DT-`namename $0`.log
-exec 1>>$LOG 2>&1
+LOG="$LOG_DIR"/"$DT"-$(namename "$0").log
+exec 1>>"$LOG" 2>&1
 
 
 if [ $DO_FETCH = 'YES' ]
@@ -38,7 +43,7 @@ then
     #1. get email with register in the body - after 05:00
     #
     #$DO fetchmail -f $CSV_DIR/.fetchmailrc -ak -m "/usr/bin/procmail -d %T"
-    $DO fetchmail -f $CSV_DIR/.fetchmailrc -k -m "/usr/bin/procmail -d %T"
+    $DO fetchmail -f "$CSV_DIR"/.fetchmailrc -k -m "/usr/bin/procmail -d %T"
 
     RC=$?
     case $RC in
@@ -53,11 +58,11 @@ then
           ;;
     esac
 
-    find $CSV_DIR -type f -name 'smime*.p7s' -delete
-    find $CSV_DIR -type f -name 'yamregister*' -size 0c -delete
+    find "$CSV_DIR" -type f -name 'smime*.p7s' -delete
+    find "$CSV_DIR" -type f -name 'yamregister*' -size 0c -delete
 
     # clean logs without mail
-    grep -l 'There was no mail' $LOG_DIR/* |xargs --no-run-if-empty rm
+    grep -l 'There was no mail' "$LOG_DIR"/* |xargs --no-run-if-empty rm
     
     if [ $exit_rc -ne 1 ]
     then
@@ -66,43 +71,59 @@ then
 fi # DO_FETCH
 
 PG_COPY_SCRIPT=$CSV_DATA/pg-COPY-registry-$DT.sql
+PG_COPY_RETURN_SCRIPT=$CSV_DATA/pg-COPY-RETURN-registry-$DT.sql
 
-pushd $CSV_DIR
+pushd "$CSV_DIR"
 
-> $PG_COPY_SCRIPT
+> "$PG_COPY_SCRIPT"
+> "$PG_COPY_RETURN_SCRIPT"
 IMPORT='NO'
 IMPORT_PAYMENT='NO'
 IMPORT_ITEM='NO'
 IFS_BCK=$IFS
 IFS=$'\n'
 #3. Prepare COPY commands for PG
-REGS_LIST=`ls -1 *yamregister*`
+REGS_LIST=$(ls -1 -- *yamregister*)
 logmsg INFO "REGS_LIST=$REGS_LIST"
 for csv1251 in $REGS_LIST
 do
-  txt=$CSV_DATA/${csv1251}.txt
-  iconv -f cp1251 -t utf8 $csv1251 |dos2unix > $txt
-  rm -f $csv1251
+  fname=$(echo "${csv1251}" |sed 's/^-*/_/g')
+  txt=$CSV_DATA/${fname}.txt
+  #txt=$CSV_DATA/${csv1251}.txt
+  iconv -f cp1251 -t utf8 -- "$csv1251" |dos2unix > "$txt"
+  ## cp $csv1251 $txt
+  rm -f -- "$csv1251"
   # import files containing not only header 
-  # input file doesn't contain CR on the last line. Use grep+wc
-  ROWS=`grep 'kipspb.ru' $txt | wc -l`
-  if [ $ROWS -gt 0 ]
+  # input file doesn't contain CR on the last line. Use grep -c
+  ROWS=$(grep -c ' kipspb.ru;' "$txt")
+  RETURN=$(grep -c 'РЕЕСТР ВОЗВРАТОВ' "$txt")
+  if [ "$ROWS" -gt 0 ] || [ "$RETURN" -gt 0 ]
   then 
-     logmsg INFO "TXT file $txt contains $ROWS rows. Prepare \\COPY command to load CSV into PG"
      # reg_no=`awk -F'№ ' '/РЕЕСТР ПЛАТЕЖЕЙ В ООО "АРКОМ". №/ {print $2}' $txt`
-     reg_date=`awk -F': ' '/Дата платежей:/ {print $2}' $txt`
+     reg_date=$(awk -F': ' '/Дата (платежей|возвратов):/ {print $2}' "$txt")
 IFS=. read loc_day loc_mon loc_year <<EODATE
 ${reg_date}
 EODATE
      PAY_DT=${loc_year}-${loc_mon}-${loc_day}
-     csv_name=$PAY_DT-`namename $txt`
-     PG_CSV=$CSV_DATA/$csv_name.csv
-     #grep 'kipspb.ru' $txt |sed -e 's/; /;/g' -e "s/$/$reg_date;/" > $PG_CSV
-     awk '/kipspb\.ru/ {N=split($0,arr,";"); for (i=1;i<=9;i++) printf "%s;", arr[i]; print ";"}' $txt |sed -e 's/; /;/g' -e "s/$/$reg_date;/" > $PG_CSV
-     mv $txt $CSV_DATA/$PAY_DT-`namename $txt`
+     csv_name=$PAY_DT-$(namename "$txt")
+     if [ "$ROWS" -gt 0 ]
+     then
+        PG_CSV=$CSV_DATA/$csv_name.csv
+        logmsg INFO "TXT file $txt contains $ROWS rows. Prepare \\COPY command to load CSV into PG"
+        awk '/kipspb\.ru/ {N=split($0,arr,";"); for (i=1;i<=9;i++) printf "%s;", arr[i]; print ";"}' "$txt" |sed -e 's/; /;/g' -e "s/$/$reg_date;/" > "$PG_CSV"
+        echo "\COPY yamregister FROM '"$PG_CSV"' WITH ( FORMAT CSV, HEADER false, DELIMITER ';') ;" >> "$PG_COPY_SCRIPT"
+        mv "$txt" "$CSV_DATA"/"$PAY_DT"-"$(namename "$txt")"
+        IMPORT='YES'
+     elif [ "$RETURN" -gt 0 ]
+     then
+        PG_CSV_RET=$CSV_DATA/$csv_name-returns.csv
+        logmsg INFO "TXT file $txt contains RETURNS rows. Prepare \\COPY command to load CSV into PG"
+        awk '/REF/ {N=split($0,arr,";"); for (i=1;i<=11;i++) printf "%s;", arr[i]; print ";"}' "$txt" |sed -e 's/; /;/g' -e "s/;$/$reg_date/" > "$PG_CSV_RET"
+        echo "\COPY yamreturn FROM '"$PG_CSV_RET"' WITH ( FORMAT CSV, HEADER false, DELIMITER ';') ;" >> "$PG_COPY_RETURN_SCRIPT"
+        mv "$txt" "$CSV_DATA"/"$PAY_DT"-"$(namename "$txt")"-returns
+        IMPORT='RETURN'
+     fi
 
-     echo "\COPY yamregister FROM '"$PG_CSV"' WITH ( FORMAT CSV, HEADER false, DELIMITER ';') ;" >> $PG_COPY_SCRIPT
-     IMPORT='YES'
   elif $(grep -q 'Извещение №' $txt)
   then
      IMPORT_PAYMENT=$CSV_DATA/${DT}-yampayments.csv
@@ -111,6 +132,19 @@ EODATE
 
      THIS_PAY_DT=`date +%F_%H_%M_%S`
      mv $txt $CSV_ARCH/$THIS_PAY_DT-`namename $txt`
+     # import
+     logmsg INFO "Import payments from $IMPORT_PAYMENT into $PG_SRV"
+     cat $IMPORT_PAYMENT
+     echo ""
+     $DO psql -h $PG_SRV -U arc_energo -d arc_energo -c '\COPY yampayment FROM '$IMPORT_PAYMENT' WITH (FORMAT CSV, DELIMITER "^", HEADER false);'
+     $DO mv $IMPORT_PAYMENT $CSV_ARCH/
+     # import items
+     logmsg INFO "Import items from $IMPORT_ITEM into $PG_SRV"
+     cat $IMPORT_ITEM
+     echo ""
+     $DO psql -h $PG_SRV -U arc_energo -d arc_energo -c '\COPY yam_item(yam_id, item_name, item_qnt, item_price) FROM '$IMPORT_ITEM' WITH (FORMAT CSV, DELIMITER "^", HEADER false);'
+     $DO mv $IMPORT_ITEM $CSV_ARCH/
+     #####################################
   else
      arch_name=$DT-`namename $txt`
      logmsg INFO "The registry $txt does not contain data row. Skip it, just archive as $arch_name"
@@ -166,14 +200,25 @@ then
       $DO mv $CSV_DATA/*yamregister* $CSV_ARCH/
       $DO mv $PG_COPY_SCRIPT $CSV_ARCH/
    fi # if RC_IMP=0 
+elif [ $IMPORT == 'RETURN' ]
+then
+   logmsg INFO "\\COPY $PG_CSV_RET into $PG_SRV"
+   cat $PG_CSV_RET
+   echo ""
+
+   $DO psql --set ON_ERROR_STOP=on -h $PG_SRV -U arc_energo -d arc_energo -w -f $PG_COPY_RETURN_SCRIPT
+   RC_IMP=$?
+   logmsg $RC_IMP "The Yandex.money registry($PG_COPY_RETURN_SCRIPT) imported."
 else
    rm -f $PG_COPY_SCRIPT
+   rm -f $PG_COPY_RETURN_SCRIPT
 fi # if IMPORT
 
 
 #5. Import payment into PG
 # use ~/.pgpass
-if [ $IMPORT_PAYMENT != 'NO' ] # csv filename
+# PROD if [ $IMPORT_PAYMENT != 'NO' ] # csv filename
+if false
 then
    logmsg INFO "Import payments from $IMPORT_PAYMENT into $PG_SRV"
    cat $IMPORT_PAYMENT
@@ -185,7 +230,8 @@ fi
 
 #6. Import items into PG
 # use ~/.pgpass
-if [ $IMPORT_ITEM != 'NO' ] # csv filename
+# PROD if [ $IMPORT_ITEM != 'NO' ] # csv filename
+if false
 then
    logmsg INFO "Import items from $IMPORT_ITEM into $PG_SRV"
    cat $IMPORT_ITEM
