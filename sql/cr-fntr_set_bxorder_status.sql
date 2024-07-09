5263e9e58a3f990bd591404e8bbceb469db1c1f1
Владимир Щербо <vscherbo@gmail.com>
2024-07-09 14:49:11 +0300

    after aub_parted

--- sql/cr-fntr_set_bxorder_status.sql
+++ sql/cr-fntr_set_bxorder_status.sql
@@ -4,7 +4,9 @@ CREATE OR REPLACE FUNCTION arc_energo.fntr_set_bxorder_status()
 AS $function$
 BEGIN
 -- 2: оплачен
-UPDATE "Счета" SET "Статус" = 2 WHERE "ИнтернетЗаказ" = NEW.order_id AND ("Статус" < 2 OR "Статус" IS NULL);
+-- OLD before aub_parted  UPDATE "Счета" SET "Статус" = 2 WHERE "ИнтернетЗаказ" = NEW.order_id AND ("Статус" < 2 OR "Статус" IS NULL);
+UPDATE "Счета" SET "Статус" = 2 WHERE "№ счета" = (SELECT "№ счета" FROM "Счета" WHERE "ИнтернетЗаказ" = new_order_id ORDER BY "№ счета" ASC LIMIT 1)
+AND ("Статус" < 2 OR "Статус" IS NULL);
 PERFORM "fn_InetOrderNewStatus"(2, NEW.order_id);
 RETURN NEW;
 END;
