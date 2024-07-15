e854212c5ebc5129d4f2c9b625f061b7def21030
Владимир Щербо <vscherbo@gmail.com>
2024-07-15 14:24:39 +0300

    single bill for bx_order

--- sql/cr-fntr_set_bxorder_status.sql
+++ sql/cr-fntr_set_bxorder_status.sql
@@ -5,7 +5,9 @@ AS $function$
 BEGIN
 -- 2: оплачен
 -- OLD before aub_parted  UPDATE "Счета" SET "Статус" = 2 WHERE "ИнтернетЗаказ" = NEW.order_id AND ("Статус" < 2 OR "Статус" IS NULL);
-UPDATE "Счета" SET "Статус" = 2 WHERE "№ счета" = (SELECT "№ счета" FROM "Счета" WHERE "ИнтернетЗаказ" = new_order_id ORDER BY "№ счета" ASC LIMIT 1)
+UPDATE "Счета" SET "Статус" = 2 WHERE 
+-- "№ счета" = (SELECT "№ счета" FROM "Счета" WHERE "ИнтернетЗаказ" = NEW.order_id ORDER BY "№ счета" ASC LIMIT 1)
+"№ счета" = (SELECT "Счет" FROM bx_order WHERE "Номер" = NEW.order_id)
 AND ("Статус" < 2 OR "Статус" IS NULL);
 PERFORM "fn_InetOrderNewStatus"(2, NEW.order_id);
 RETURN NEW;
